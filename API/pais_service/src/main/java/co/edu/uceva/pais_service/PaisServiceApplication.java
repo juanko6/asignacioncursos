db697ad54f959ac2a8a09dece48114e764641829
Juan Gutierrez <juanko6@gmail.com>
2023-11-22 14:38:01 -0500

    actualiza entrada PaisServiceApplication para que cargue biblioteca proyecto_grafos

--- API/pais_service/src/main/java/co/edu/uceva/pais_service/PaisServiceApplication.java
+++ API/pais_service/src/main/java/co/edu/uceva/pais_service/PaisServiceApplication.java
@@ -3,9 +3,13 @@ package co.edu.uceva.pais_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PaisServiceApplication {
 
+    static {
+        System.loadLibrary("proyecto_grafos");
+    }
     public static void main(String[] args) {
         SpringApplication.run(PaisServiceApplication.class, args);
     }
