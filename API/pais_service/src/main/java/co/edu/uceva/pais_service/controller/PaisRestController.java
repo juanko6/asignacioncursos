3a90cc44a0d310b6fb916ff49fbd7ee485908c10
Juan Gutierrez <juanko6@gmail.com>
2023-11-22 14:46:22 -0500

    se crea metodo POST en restcontroller que recibe un string con el jsoncursos y retorna el string de funcionasignacion

--- API/pais_service/src/main/java/co/edu/uceva/pais_service/controller/PaisRestController.java
+++ API/pais_service/src/main/java/co/edu/uceva/pais_service/controller/PaisRestController.java
@@ -2,6 +2,7 @@ package co.edu.uceva.pais_service.controller;
 
 import co.edu.uceva.pais_service.model.entities.Pais;
 import co.edu.uceva.pais_service.model.service.IPaisService;
+import co.edu.uceva.pais_service.model.service.grafos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,9 +29,21 @@ public class PaisRestController {
         return paisService.save(pais);
     }
 
-    @GetMapping("/hola/{nombre}") //La fomra en que llamo este metodo (ENDPOINT)
+    @GetMapping("/hola/{nombre}") //La forma en que llamo este metodo (ENDPOINT)
     public String holaMundo(@PathVariable("nombre") String nombre){
         return "Hola " + nombre;
     }
+
+    @PostMapping("/asignarcursos") //
+    public String asignarCursos(@RequestBody String jsonCursos)
+    {
+        grafos grafos = new grafos();
+        return grafos.funcionAsignacion(jsonCursos, jsonSalas);
+    }
+
+    String jsonSalas = "[{\"nombre\": \"Sala A\",\"capacidad\": 30},"
+            + "{\"nombre\": \"Sala B\",\"capacidad\": 35},"
+            + "{\"nombre\": \"Sala C\",\"capacidad\": 40 }]";
+
 
 }
